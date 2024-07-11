8ead7f21921f2e1290c7f61098a21e9b380c2e61
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-07-11 20:01:53 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
-}
+    document
+        .getElementById("button")
+        .addEventListener("click", () => alert("I have been clicked"));
+    }
+    addingEventListener()
